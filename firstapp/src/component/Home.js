6a1e06f41a15041d2265a9fa42715e89927ebc0e
Aakashdeveloper <ahanda205@gmail.com>
2020-11-06 22:41:07 +0530

    a

--- firstapp/src/component/Home.js
+++ firstapp/src/component/Home.js
@@ -1,14 +1,28 @@
 import React,{Component} from 'react';
 import Footer from './Footer';
 import Header from './Header';
+import JSON from './db.json';
+import NewsDisplay from './NewsDisplay';
 
 class Home extends Component {
+    constructor(){
+        super()
+
+        this.state={
+            news:JSON
+        }
+    }
+
+    filterNews = (keyword) => {
+        console.log("in home ",keyword)
+    }
+
     render(){
+        console.log(JSON)
         return(
             <React.Fragment>
-                <Header/>
-                <h1>React App</h1>
-                <h2>My App</h2>
+                <Header userText={(userInput)=>{this.filterNews(userInput)}}/>
+                <NewsDisplay newsdata={this.state.news}/>
                 <Footer year="2021" month="Nov"/>
             </React.Fragment>
         )
