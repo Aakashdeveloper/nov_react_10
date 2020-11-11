de49ffebc320a48179fec94e691cc43908affce1
Aakashdeveloper <ahanda205@gmail.com>
2020-11-11 22:39:09 +0530

    a

--- routing/src/component/Post.js
+++ routing/src/component/Post.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Post = () => {
     return(
@@ -8,6 +9,18 @@ const Post = () => {
             </div>
             <div className="panel-body">
                <p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book.</p>
+                <div>
+                    <h4>JavaScript</h4>
+                    <Link to="/post/JavaScript">Details</Link>
+                </div>
+                <div>
+                    <h4>NodeJS</h4>
+                    <Link to="/post/NodeJs">Details</Link>
+                </div>
+                <div>
+                    <h4>ReactJs</h4>
+                    <Link to="/post/ReactJs">Details</Link>
+                </div>
             </div>
         </div>
     )
--- routing/src/component/Routing.js
+++ routing/src/component/Routing.js
@@ -5,13 +5,15 @@ import Post from './Post';
 import Profile from './Profile';
 import Header from './Header';
 import Footer from './Footer';
+import PostDetails from './PostDetails'
 
 const Routing = () => {
      return(
         <BrowserRouter>
             <Header/>
             <Route exact path="/" component={Home}></Route>
-            <Route path="/post" component={Post}></Route>
+            <Route exact path="/post" component={Post}></Route>
+            <Route path="/post/:topic" component={PostDetails}></Route>
             <Route path="/profile" component={Profile}></Route>
             <Footer/>
         </BrowserRouter>
