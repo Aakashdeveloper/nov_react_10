75f87b938001c9bd5c9b71f3f24a0551216e9b11
aakashdeveloper <ahanda205@gmail.com>
2020-11-26 22:38:54 +0530

    a

--- graphqlfront/src/index.js
+++ graphqlfront/src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ApolloClient from 'apollo-boost';
+import gql from 'graphql-tag';
+import {Query} from 'react-apollo';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const client = new ApolloClient({
+    uri:'http://localhost:8600/graphql'
+});
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const GET_REST = gql`
+    query product($id:Int){
+        product(id:$id){
+            name,
+            rating_text,
+            locality
+        }
+    }
+`
+
+class Rest extends Component{
+    constructor(){
+        super()
+
+        this.state={
+            id:2
+        }
+    }
+
+    render(){
+        return(
+            <Query query={GET_REST} client={client} variables={{id:this.state.id}}>
+                {({loading,err,data}) => {
+                    if(loading) return <p>Loading....</p>
+                    if(err) return <p>Err....</p>
+                    return(
+                        <div>
+                            <h2>{data.product.name}</h2>
+                            <h2>{data.product.rating_text}</h2>
+                            <h2>{data.product.locality}</h2>
+                        </div>
+                    )
+                }}
+
+            </Query>
+        )
+    }
+}
+
+ReactDOM.render(<Rest/>,document.getElementById('root'))
